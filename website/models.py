248db1984e9337348b7d8c8425de5d5e6ddaff76
batayneh <batayneh.03@gmail.com>
2025-05-12 08:09:16 +0300

    nset

--- website/models.py
+++ website/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class MyApp(models.Model):
     name = models.CharField(max_length=60, null=False)
     description = models.TextField(max_length=500, blank=True, default='')
+    image = models.ImageField()
     src = models.TextField(blank=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='apps', null=True)
